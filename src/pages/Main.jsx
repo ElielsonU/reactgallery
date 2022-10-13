9a7b7ff090fe5bad8f3b728c93e5a238ddd2a35f
Elielson Urbano <elielson7756@gmail.com>
2022-10-12 23:50:15 -0300

    Initial Prototype

---
+++ src/pages/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+class MainPage extends React.Component {
+    render() {
+        return (
+        <React.Fragment>
+            <form action="">
+            <input type="file" name="" id="" />
+            </form>
+            <a href=""></a>
+        </React.Fragment>
+        )
+    }
+}
+
+export default MainPage 
